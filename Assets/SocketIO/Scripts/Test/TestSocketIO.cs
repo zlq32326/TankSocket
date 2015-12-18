e359fae13a8ed265080a1c98200a4470a3596640
zlq <zhoulinqiang@haishengyue.com>
2015-12-18 23:15:22 +0800

    update

--- Assets/SocketIO/Scripts/Test/TestSocketIO.cs
+++ Assets/SocketIO/Scripts/Test/TestSocketIO.cs
@@ -34,68 +34,79 @@ public class TestSocketIO : MonoBehaviour
 {
 	private SocketIOComponent socket;
 
-	public void Start() 
+	public void Start ()
 	{
-		GameObject go = GameObject.Find("SocketIO");
-		socket = go.GetComponent<SocketIOComponent>();
+		GameObject go = GameObject.Find ("SocketIO");
+		socket = go.GetComponent<SocketIOComponent> ();
 
-		socket.On("open", TestOpen);
-		socket.On("boop", TestBoop);
-		socket.On("error", TestError);
-		socket.On("close", TestClose);
+		socket.On ("open", TestOpen);
+		socket.On ("boop", TestBoop);
+		socket.On ("error", TestError);
+		socket.On ("close", TestClose);
+		socket.On ("connection", TestConnection);
 		
-		StartCoroutine("BeepBoop");
+		StartCoroutine ("BeepBoop");
 	}
 
-	private IEnumerator BeepBoop()
+	private IEnumerator BeepBoop ()
 	{
+		socket.Emit ("connection");
 		// wait 1 seconds and continue
-		yield return new WaitForSeconds(1);
+		yield return new WaitForSeconds (1);
 		
-		socket.Emit("beep");
+		socket.Emit ("beep");
 		
 		// wait 3 seconds and continue
-		yield return new WaitForSeconds(3);
+		yield return new WaitForSeconds (3);
 		
-		socket.Emit("beep");
+		socket.Emit ("beep");
 		
 		// wait 2 seconds and continue
-		yield return new WaitForSeconds(2);
+		yield return new WaitForSeconds (2);
 		
-		socket.Emit("beep");
+		socket.Emit ("beep");
 		
 		// wait ONE FRAME and continue
 		yield return null;
 		
-		socket.Emit("beep");
-		socket.Emit("beep");
+		socket.Emit ("beep");
+		socket.Emit ("beep");
+
+
 	}
 
-	public void TestOpen(SocketIOEvent e)
+	public void TestOpen (SocketIOEvent e)
 	{
-		Debug.Log("[SocketIO] Open received: " + e.name + " " + e.data);
+		Debug.Log ("[SocketIO] Open received: " + e.name + " " + e.data);
 	}
-	
-	public void TestBoop(SocketIOEvent e)
+
+	public void TestBoop (SocketIOEvent e)
 	{
-		Debug.Log("[SocketIO] Boop received: " + e.name + " " + e.data);
+		Debug.Log ("[SocketIO] Boop received: " + e.name + " " + e.data);
 
-		if (e.data == null) { return; }
+		if (e.data == null) {
+			return;
+		}
 
-		Debug.Log(
+		Debug.Log (
 			"#####################################################" +
-			"THIS: " + e.data.GetField("this").str +
+			"THIS: " + e.data.GetField ("this").str +
 			"#####################################################"
 		);
 	}
-	
-	public void TestError(SocketIOEvent e)
+
+	public void TestError (SocketIOEvent e)
 	{
-		Debug.Log("[SocketIO] Error received: " + e.name + " " + e.data);
+		Debug.Log ("[SocketIO] Error received: " + e.name + " " + e.data);
 	}
-	
-	public void TestClose(SocketIOEvent e)
+
+	public void TestClose (SocketIOEvent e)
 	{	
-		Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
+		Debug.Log ("[SocketIO] Close received: " + e.name + " " + e.data);
+	}
+
+	public void TestConnection (SocketIOEvent e)
+	{
+		Debug.Log ("laallalalallalalalalalalalalalal~~~~~~~~~"); 
 	}
 }
